9c08c0cd209c7cff99d7dc16655fafc9633428e7
Alejandro Puche Velasco <apuc@ciklum.com>
2025-01-22 18:00:50 +0100

    to clear screen when players are ready

--- energykids/src/scenes/MinigameIntro.ts
+++ energykids/src/scenes/MinigameIntro.ts
@@ -21,6 +21,9 @@ export class MinigameIntro extends Scene {
 
     players: Player[] = []
 
+    gameTitle: Phaser.GameObjects.Text
+    gameDescription: Phaser.GameObjects.Text
+
     constructor() {
         super('MinigameIntro')
     }
@@ -33,8 +36,15 @@ export class MinigameIntro extends Scene {
 
         this.background = this.add.image(512, 384, 'pb_background')
 
-        // Add Title
-        this.add.text(0, 150, this.config.title, {
+        // Add Game Title
+        this.add.text(50, 50, 'Save the city - \nBe the hero', {
+            fontFamily: 'MightySoul',
+            fontSize: '50px',
+            color: '#ABCFFB',
+        })
+
+        // Add MiniGame Title
+        this.gameTitle = this.add.text(0, 150, this.config.title, {
             align: 'center',
             fixedWidth: 1024,
             fontSize: '48px',
@@ -43,7 +53,7 @@ export class MinigameIntro extends Scene {
         })
 
         // Add Description
-        this.add.text(
+        this.gameDescription = this.add.text(
             100,
             +this.game.config.height / 2 - 150,
             this.config.tutorialText,
@@ -109,7 +119,7 @@ export class MinigameIntro extends Scene {
                 }
             )
 
-            const characterKey = `character${i+1}`
+            const characterKey = `character${i + 1}`
             this.add.image(
                 position.x,
                 position.y - 100,
@@ -140,16 +150,19 @@ export class MinigameIntro extends Scene {
                     this.time.delayedCall(500, () => {
                         this.add.text(
                             0,
-                            +this.game.config.height / 2,
+                            +this.game.config.height / 2 - 100,
                             'Get Ready!',
                             {
                                 align: 'center',
                                 fixedWidth: 1024,
-                                fontSize: 45,
-                                color: 'white',
+                                fontSize: 70,
+                                color: '#00F077',
                                 fontStyle: 'bold',
+                                fontFamily: 'MightySoul',
                             }
                         )
+                        this.gameTitle.setVisible(false)
+                        this.gameDescription.setVisible(false)
                         this.time.delayedCall(2000, () => {
                             this.cameras.main.fadeOut(1000, 0, 0, 0)
                             this.cameras.main.once(
