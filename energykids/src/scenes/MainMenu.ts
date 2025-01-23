bdc03af17d92bf1ac981ec87ef109ac5c6394645
Alejandro Puche Velasco <apuc@ciklum.com>
2025-01-23 10:30:56 +0100

    Starting screen scene

--- energykids/src/scenes/MainMenu.ts
+++ energykids/src/scenes/MainMenu.ts
@@ -1,32 +1,68 @@
 import { Scene, GameObjects } from 'phaser'
 
 export class MainMenu extends Scene {
-    background: GameObjects.Image
+    background: Phaser.GameObjects.Image
     logo: GameObjects.Image
     title: GameObjects.Text
+    description: GameObjects.Text
+    buttonText: GameObjects.Text
 
     constructor() {
         super('MainMenu')
     }
 
     create() {
-        // this.background = this.add.image(512, 384, 'background')
+        this.background = this.add.image(512, 384, 'city_background1')
 
-        // this.logo = this.add.image(512, 300, 'logo')
+        //this.logo = this.add.image(512, 300, 'logo')
 
         this.title = this.add
-            .text(512, 460, 'Main Menuu', {
-                fontFamily: 'Arial Black',
-                fontSize: 38,
-                color: '#ffffff',
-                stroke: '#000000',
-                strokeThickness: 8,
+            .text(512, 280, 'Save the city -\n Be the hero', {
                 align: 'center',
+                fontSize: 80,
+                color: '#00F077',
+                fontStyle: 'bold',
+                fontFamily: 'MightySoul',
             })
             .setOrigin(0.5)
 
-        this.input.once('pointerdown', () => {
-            this.scene.start('CatchGame')
-        })
+        this.description = this.add
+            .text(
+                512,
+                440,
+                'The city is doing badly. Help us save her!\n Play little mini-games with us and watch the city \n gradualy improve. ',
+                {
+                    align: 'center',
+                    fontSize: 30,
+                    color: '#ABCFFB',
+                    fontStyle: 'bold',
+                    fontFamily: 'MightySoul',
+                }
+            )
+            .setOrigin(0.5)
+
+            const startButton = this.add
+            .image(512, 600, 'button_start')
+            .setInteractive()
+            .on('pointerover', () => {
+                startButton.setTexture('button_start_hover')
+            })
+            .on('pointerout', () => {
+                startButton.setTexture('button_start')
+            })
+            .on('pointerdown', () => {
+                this.scene.start('Lobby')
+            })
+
+            this.buttonText = this.add
+                .text(512, 595, 'Let´s go!', {
+                    align: 'center',
+                    fontSize: 40,
+                    color: '#0165E4',
+                    fontStyle: 'bold',
+                    fontFamily: 'MightySoul',
+                })
+                .setOrigin(0.5)
+        }
     }
-}
+    
